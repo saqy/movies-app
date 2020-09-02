6665e1caecacc49261f06f910deb3ddc0ecb11d5
Muhammad Saqib <muhammadsaqiib@gmail.com>
2020-09-02 11:42:49 -0400

    Updates

---
+++ precache-manifest.0f79d40abe9014ebe9a90316e33029b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bc30fdff96db95afeb52f5114a9740",
+    "url": "https://saqy.github.io/movies-app/index.html"
+  },
+  {
+    "revision": "e75472242979f3ab5631",
+    "url": "https://saqy.github.io/movies-app/static/js/2.4aa92449.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "https://saqy.github.io/movies-app/static/js/2.4aa92449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77bb61c32f8b8b5dc3f",
+    "url": "https://saqy.github.io/movies-app/static/js/main.202d6475.chunk.js"
+  },
+  {
+    "revision": "24ebae12bde168747764",
+    "url": "https://saqy.github.io/movies-app/static/js/runtime-main.fb3f5ce2.js"
+  }
+]);
